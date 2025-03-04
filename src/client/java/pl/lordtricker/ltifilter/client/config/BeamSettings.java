e01b1866f73fe005d64e37c7fcbcd5050adc8e5d
LordTricker <oskar@blazecode.pl>
2025-03-04 18:35:59 +0100

    edited beam version

--- src/client/java/pl/lordtricker/ltifilter/client/config/BeamSettings.java
+++ src/client/java/pl/lordtricker/ltifilter/client/config/BeamSettings.java
@@ -4,11 +4,11 @@ public class BeamSettings {
     // Kolor zapisany w formacie hex (np. "#80ccff")
     public String hexColor = "#80ccff";
     // Przezroczystość beama
-    public float alpha = 1.0f;
+    public float alpha = 0.7f;
     // Wysokość słupa beama
-    public float height = 1.0f;
+    public float height = 0.65f;
     // Promień beama
-    public float radius = 0.05f;
+    public float radius = 0.04f;
     // Przesunięcie pionowe beama od ziemi (np. 0.5 - beam zacznie się 0.5 bloku nad ziemią)
-    public float verticalOffset = 0.0f;
+    public float verticalOffset = 0.55f;
 }
