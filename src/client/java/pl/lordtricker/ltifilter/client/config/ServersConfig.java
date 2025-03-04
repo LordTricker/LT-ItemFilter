34f5da8890fbe6343753ad801fb3772faa9e76cd
LordTricker <oskar@blazecode.pl>
2025-03-04 17:48:36 +0100

    support for version 1.21.4

--- src/client/java/pl/lordtricker/ltifilter/client/config/ServersConfig.java
+++ src/client/java/pl/lordtricker/ltifilter/client/config/ServersConfig.java
@@ -6,4 +6,7 @@ import java.util.List;
 public class ServersConfig {
     public String defaultProfile = "default";
     public List<ServerEntry> servers = new ArrayList<>();
+
+    public BeamSettings beamSettings = new BeamSettings();
+    public CleanerSettings cleanerSettings = new CleanerSettings();
 }
