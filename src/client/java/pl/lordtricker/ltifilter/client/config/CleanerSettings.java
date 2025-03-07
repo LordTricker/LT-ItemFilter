9e8b92d73d17755455947392dd53dcde207adef8
LordTricker <oskar@blazecode.pl>
2025-03-07 18:40:22 +0100

    Big update to version 1.1.0 added max items in eq added simplier adding items added better list of items added slots in config to no-drop

--- src/client/java/pl/lordtricker/ltifilter/client/config/CleanerSettings.java
+++ src/client/java/pl/lordtricker/ltifilter/client/config/CleanerSettings.java
@@ -1,9 +1,14 @@
 package pl.lordtricker.ltifilter.client.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CleanerSettings {
     public int throwIntervalTicks = 2;
     public long blockDurationMs = 200;
 
-//    public String pickupSound = "minecraft:entity.experience_orb.pickup";
-//    public int pickupSoundDelayTicks = 2;
+    //    public String pickupSound = "minecraft:entity.experience_orb.pickup";
+    //    public int pickupSoundDelayTicks = 2;
+
+    public List<Integer> doNotCleanSlots = new ArrayList<>();
 }
