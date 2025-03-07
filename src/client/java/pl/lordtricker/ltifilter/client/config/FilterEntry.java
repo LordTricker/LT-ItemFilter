c1d8b1b7d0719ce5939a7c26277570ba90175241
LordTricker <oskar@blazecode.pl>
2025-03-07 18:32:04 +0100

    Big update to version 1.1.0 added max items in eq added simplier adding items added better list of items added slots in config to no-drop

--- src/client/java/pl/lordtricker/ltifilter/client/config/FilterEntry.java
+++ src/client/java/pl/lordtricker/ltifilter/client/config/FilterEntry.java
@@ -2,8 +2,23 @@ package pl.lordtricker.ltifilter.client.config;
 
 public class FilterEntry {
     public String material;
+    public int maxCount = -1;
 
     public FilterEntry(String material) {
         this.material = material;
+    }
+
+    public FilterEntry(String material, int maxCount) {
+        this.material = material;
+        this.maxCount = maxCount;
+    }
+
+    @Override
+    public String toString() {
+        if (maxCount <= 0) {
+            return material + " (bez limitu)";
+        } else {
+            return material + " (max: " + maxCount + ")";
+        }
     }
 }
