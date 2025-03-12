478e14c3129e8aae9adbd6d5786ff4cb48b2f171
LordTricker <oskar@blazecode.pl>
2025-03-12 19:43:34 +0100

    Big update with new params in command and adding by hand items with enchants and custom names

--- src/client/java/pl/lordtricker/ltifilter/client/config/ConfigLoader.java
+++ src/client/java/pl/lordtricker/ltifilter/client/config/ConfigLoader.java
@@ -13,11 +13,24 @@ import java.util.List;
 
 public class ConfigLoader {
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-    private static final String CONFIG_FILE_NAME = "ltitemfilter-config.json";
+    private static final String MAIN_CONFIG_FILE_NAME = "ltitemfilter-config.json";
+    private static final Path MOD_CONFIG_DIR;
+
+    static {
+        Path configDir = FabricLoader.getInstance().getConfigDir();
+        MOD_CONFIG_DIR = configDir.resolve("LT-Mods").resolve("LT-ItemFilter");
+        try {
+            if (!Files.exists(MOD_CONFIG_DIR)) {
+                Files.createDirectories(MOD_CONFIG_DIR);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static ServersConfig loadConfig() {
         Path configDir = FabricLoader.getInstance().getConfigDir();
-        Path configFile = configDir.resolve(CONFIG_FILE_NAME);
+        Path configFile = configDir.resolve(MAIN_CONFIG_FILE_NAME);
 
         if (!Files.exists(configFile)) {
             ServersConfig defaultConfig = createDefaultConfig();
@@ -26,7 +39,9 @@ public class ConfigLoader {
         }
 
         try (Reader reader = Files.newBufferedReader(configFile)) {
-            return GSON.fromJson(reader, ServersConfig.class);
+            ServersConfig loadedConfig = GSON.fromJson(reader, ServersConfig.class);
+            fixNullFields(loadedConfig);
+            return loadedConfig;
         } catch (IOException e) {
             e.printStackTrace();
             return new ServersConfig();
@@ -35,12 +50,25 @@ public class ConfigLoader {
 
     public static void saveConfig(ServersConfig config) {
         Path configDir = FabricLoader.getInstance().getConfigDir();
-        Path configFile = configDir.resolve(CONFIG_FILE_NAME);
+        Path configFile = configDir.resolve(MAIN_CONFIG_FILE_NAME);
 
         try (Writer writer = Files.newBufferedWriter(configFile)) {
             GSON.toJson(config, writer);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void fixNullFields(ServersConfig config) {
+        if (config.servers == null) return;
+        for (ServerEntry serverEntry : config.servers) {
+            if (serverEntry.filters == null) continue;
+            for (FilterEntry fe : serverEntry.filters) {
+                if (fe.baseName == null)   fe.baseName = "";
+                if (fe.lore == null)       fe.lore = "";
+                if (fe.material == null)   fe.material = "";
+                if (fe.enchants == null)   fe.enchants = "";
+            }
         }
     }
 
@@ -62,44 +90,44 @@ public class ConfigLoader {
         server1.domains = List.of("minestar.pl", "anarchia.gg", "tryhc.net", "rapy.pl");
         server1.profileName = "pvp";
 
-        server1.filters.add(new FilterEntry("minecraft:diamond_pickaxe", -1));
-        server1.filters.add(new FilterEntry("minecraft:diamond_axe", -1));
-        server1.filters.add(new FilterEntry("minecraft:diamond_sword", -1));
-        server1.filters.add(new FilterEntry("minecraft:diamond_helmet", -1));
-        server1.filters.add(new FilterEntry("minecraft:diamond_chestplate", -1));
-        server1.filters.add(new FilterEntry("minecraft:diamond_leggings", -1));
-        server1.filters.add(new FilterEntry("minecraft:diamond_boots", -1));
-        server1.filters.add(new FilterEntry("minecraft:netherite_pickaxe", -1));
-        server1.filters.add(new FilterEntry("minecraft:netherite_axe", -1));
-        server1.filters.add(new FilterEntry("minecraft:netherite_sword", -1));
-        server1.filters.add(new FilterEntry("minecraft:netherite_helmet", -1));
-        server1.filters.add(new FilterEntry("minecraft:netherite_chestplate", -1));
-        server1.filters.add(new FilterEntry("minecraft:netherite_leggings", -1));
-        server1.filters.add(new FilterEntry("minecraft:netherite_boots", -1));
-        server1.filters.add(new FilterEntry("minecraft:golden_apple", -1));
-        server1.filters.add(new FilterEntry("minecraft:enchanted_golden_apple", -1));
-        server1.filters.add(new FilterEntry("minecraft:ender_pearl", -1));
-        server1.filters.add(new FilterEntry("minecraft:enchanted_book", -1));
-        server1.filters.add(new FilterEntry("minecraft:bow", -1));
-        server1.filters.add(new FilterEntry("minecraft:crossbow", -1));
-        server1.filters.add(new FilterEntry("minecraft:golden_helmet", -1));
-        server1.filters.add(new FilterEntry("minecraft:fireworks", 4));
-        server1.filters.add(new FilterEntry("minecraft:elytra", -1));
-        server1.filters.add(new FilterEntry("minecraft:trident", -1));
-        server1.filters.add(new FilterEntry("minecraft:shulker_box", -1));
-        server1.filters.add(new FilterEntry("minecraft:snowball", 4));
-        server1.filters.add(new FilterEntry("minecraft:red_dye", -1));
-        server1.filters.add(new FilterEntry("minecraft:arrow", -1));
-        server1.filters.add(new FilterEntry("minecraft:chorus_fruit", -1));
-        server1.filters.add(new FilterEntry("minecraft:slime_block", 2));
-        server1.filters.add(new FilterEntry("minecraft:obsidian", 2));
+        server1.filters.add(new FilterEntry("diamond pickaxe", "", "minecraft:diamond_pickaxe", "", -1));
+        server1.filters.add(new FilterEntry("diamond axe", "", "minecraft:diamond_axe", "", -1));
+        server1.filters.add(new FilterEntry("diamond sword", "", "minecraft:diamond_sword", "", -1));
+        server1.filters.add(new FilterEntry("diamond helmet", "", "minecraft:diamond_helmet", "", -1));
+        server1.filters.add(new FilterEntry("diamond chestplate", "", "minecraft:diamond_chestplate", "", -1));
+        server1.filters.add(new FilterEntry("diamond leggings", "", "minecraft:diamond_leggings", "", -1));
+        server1.filters.add(new FilterEntry("diamond boots", "", "minecraft:diamond_boots", "", -1));
+        server1.filters.add(new FilterEntry("netherite pickaxe", "", "minecraft:netherite_pickaxe", "", -1));
+        server1.filters.add(new FilterEntry("netherite axe", "", "minecraft:netherite_axe", "", -1));
+        server1.filters.add(new FilterEntry("netherite sword", "", "minecraft:netherite_sword", "", -1));
+        server1.filters.add(new FilterEntry("netherite helmet", "", "minecraft:netherite_helmet", "", -1));
+        server1.filters.add(new FilterEntry("netherite chestplate", "", "minecraft:netherite_chestplate", "", -1));
+        server1.filters.add(new FilterEntry("netherite leggings", "", "minecraft:netherite_leggings", "", -1));
+        server1.filters.add(new FilterEntry("netherite boots", "", "minecraft:netherite_boots", "", -1));
+        server1.filters.add(new FilterEntry("golden apple", "", "minecraft:golden_apple", "", -1));
+        server1.filters.add(new FilterEntry("enchanted golden apple", "", "minecraft:enchanted_golden_apple", "", -1));
+        server1.filters.add(new FilterEntry("ender pearl", "", "minecraft:ender_pearl", "", -1));
+        server1.filters.add(new FilterEntry("enchanted book", "", "minecraft:enchanted_book", "", -1));
+        server1.filters.add(new FilterEntry("bow", "", "minecraft:bow", "", -1));
+        server1.filters.add(new FilterEntry("crossbow", "", "minecraft:crossbow", "", -1));
+        server1.filters.add(new FilterEntry("golden helmet", "", "minecraft:golden_helmet", "", -1));
+        server1.filters.add(new FilterEntry("fireworks", "", "minecraft:fireworks", "", 4));
+        server1.filters.add(new FilterEntry("elytra", "", "minecraft:elytra", "", -1));
+        server1.filters.add(new FilterEntry("trident", "", "minecraft:trident", "", -1));
+        server1.filters.add(new FilterEntry("shulker box", "", "minecraft:shulker_box", "", -1));
+        server1.filters.add(new FilterEntry("snowball", "", "minecraft:snowball", "", 4));
+        server1.filters.add(new FilterEntry("red dye", "", "minecraft:red_dye", "", -1));
+        server1.filters.add(new FilterEntry("arrow", "", "minecraft:arrow", "", -1));
+        server1.filters.add(new FilterEntry("chorus fruit", "", "minecraft:chorus_fruit", "", -1));
+        server1.filters.add(new FilterEntry("slime block", "", "minecraft:slime_block", "", 2));
+        server1.filters.add(new FilterEntry("obsidian", "", "minecraft:obsidian", "", 2));
 
         cfg.servers.add(server1);
 
         ServerEntry server2 = new ServerEntry();
         server2.domains = List.of("kokscraft.pl");
         server2.profileName = "bedwars";
-        server2.filters.add(new FilterEntry("minecraft:netherite_sword"));
+        server2.filters.add(new FilterEntry("netherite sword", "", "minecraft:netherite_sword", "", -1));
 
         cfg.servers.add(server2);
 
--- src/client/java/pl/lordtricker/ltifilter/client/config/FilterEntry.java
+++ src/client/java/pl/lordtricker/ltifilter/client/config/FilterEntry.java
@@ -1,24 +1,45 @@
 package pl.lordtricker.ltifilter.client.config;
 
 public class FilterEntry {
+    public String baseName;
+    public String lore;
     public String material;
+    public String enchants;
     public int maxCount = -1;
 
-    public FilterEntry(String material) {
-        this.material = material;
+    public FilterEntry(String baseName, String lore, String material, String enchants, int maxCount) {
+        this.baseName = baseName == null ? "" : baseName;
+        this.lore = lore == null ? "" : lore;
+        this.material = material == null ? "" : material;
+        this.enchants = enchants == null ? "" : enchants;
+        this.maxCount = maxCount;
     }
 
     public FilterEntry(String material, int maxCount) {
-        this.material = material;
-        this.maxCount = maxCount;
+        this(material, "", material, "", maxCount);
     }
 
     @Override
     public String toString() {
-        if (maxCount <= 0) {
-            return material + " (bez limitu)";
-        } else {
-            return material + " (max: " + maxCount + ")";
+        StringBuilder sb = new StringBuilder();
+        sb.append(baseName);
+        if (enchants != null && !enchants.isEmpty()) {
+            sb.append(" {\"").append(enchants).append("\"}");
         }
+        if (material != null && !material.isEmpty() && !baseName.equalsIgnoreCase(material)) {
+            String displayMaterial = material.toLowerCase().startsWith("minecraft:")
+                    ? material.substring("minecraft:".length())
+                    : material;
+            sb.append(" [\"").append(displayMaterial).append("\"]");
+        }
+        if (lore != null && !lore.isEmpty()) {
+            sb.append(" (\"").append(lore).append("\")");
+        }
+        if (maxCount <= 0) {
+            sb.append(" (bez limitu)");
+        } else {
+            sb.append(" (max: ").append(maxCount).append(")");
+        }
+        return sb.toString().trim();
     }
 }
